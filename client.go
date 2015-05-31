f6732eb40631a0910b57c343b1d72cc6fbf81817
Aleksandr Razumov <arazumov@ddestiny.ru>
2015-06-01 01:38:55 +0300

    initial implementation

---
+++ client.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"crypto/sha1"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
+)
+
+const (
+	clientAPIHost      = "rpc.hentaiathome.net"
+	clientAPIScheme    = "http"
+	httpGET            = "get"
+	clientAPIPath      = "clientapi.php"
+	clientBuild        = 96
+	clientVersion      = "1.2.25"
+	argClientBuild     = "clientbuild"
+	argAction          = "act"
+	argClientID        = "cid"
+	argActionArgument  = "add"
+	argActionKey       = "actkey"
+	argTime            = "acttime"
+	actionKeyStart     = "hentai@home"
+	actionKeyDelimiter = "-"
+	actionStart        = "client_start"
+)
+
+type APIResponce struct {
+	Success bool
+	Message string
+	Data    string
+}
+
+// Client is api for hath rpc
+type Client struct {
+	id         int64
+	key        string
+	httpClient *http.Client
+}
+
+func (c Client) getURL(args ...string) *url.URL {
+	// preparing time and arguments
+	if len(args) == 0 {
+		panic("bad arguments lenght in getServerAPIURL")
+	}
+	unixTime := time.Now().Unix()
+	action := args[0]
+	argument := ""
+	if len(args) > 1 {
+		argument = args[1]
+	}
+
+	// building action key
+	toHash := strings.Join([]string{actionKeyStart,
+		action,
+		argument,
+		fmt.Sprint(c.id),
+		fmt.Sprint(unixTime),
+		c.key},
+		actionKeyDelimiter,
+	)
+	h := sha1.New()
+	fmt.Fprint(h, toHash)
+	actionKey := fmt.Sprintf("%x", h.Sum(nil))
+
+	// building url
+	u := &url.URL{Scheme: clientAPIScheme, Path: clientAPIPath, Host: clientAPIHost}
+	values := make(url.Values)
+	values.Add(argClientBuild, fmt.Sprint(clientBuild))
+	values.Add(argAction, action)
+	values.Add(argActionArgument, argument)
+	values.Add(argActionKey, actionKey)
+	values.Add(argTime, fmt.Sprint(unixTime))
+	values.Add(argClientID, fmt.Sprint(c.id))
+	u.RawQuery = values.Encode()
+	return u
+}
+
+// ActionURL - get url for action
+func (c Client) ActionURL(action string) *url.URL {
+	return c.getURL(action)
+}
+
+func (c Client) getResponce(action string) (r APIResponce, err error) {
+	return
+}
+
+func (c Client) printRequest(action string) {
+	u := c.ActionURL(action)
+	log.Println(httpGET, u)
+	res, err := c.httpClient.Get(u.String())
+	if err != nil {
+		log.Print(err)
+	}
+	defer res.Body.Close()
+	data, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Print(err)
+	}
+	log.Println(string(data))
+}
+
+// NewClient creates new client for api
+func NewClient(id int64, key string) *Client {
+	c := new(Client)
+	c.id = id
+	c.key = key
+	c.httpClient = http.DefaultClient
+	return c
+}
--- main.go
+++ main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 )
 
+var (
+	clientID  int64
+	clientKey string
+)
+
+func init() {
+	flag.Int64Var(&clientID, "client-id", 0, "Hentai@Home client id")
+	flag.StringVar(&clientKey, "client-key", "", "Hentai@Home client key")
+}
+
 func main() {
+	flag.Parse()
 	fmt.Println("Hello, hath!")
+	client := NewClient(clientID, clientKey)
+	client.printRequest(actionStart)
+	fmt.Println(client.ActionURL("test"))
 }
