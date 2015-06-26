c3b2c02b880919f83e4103f3f8d31e9759cf8c29
Александр Разумов <arazumov@ddestiny.ru>
2015-06-26 21:59:14 +0300

    frontend: updated prototype

--- hath-frontend/main.go
+++ hath-frontend/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"net/http"
 
+	"cydev.ru/hath"
+
 	"github.com/GeertJohan/go.rice"
 	"github.com/gin-gonic/gin"
 )
@@ -20,10 +22,20 @@ func static(c *gin.Context) {
 	http.FileServer(box.HTTPBox()).ServeHTTP(c.Writer, c.Request)
 }
 
+func stats(c *gin.Context) {
+	stats := hath.Stats{}
+	stats.FilesDownloaded = 1024
+	stats.FilesSent = 512
+	stats.FilesDownloadedBytes = 1024 * 1024 * 50
+	stats.FilesSentBytes = 1024 * 1024 * 25
+	c.JSON(http.StatusOK, stats)
+}
+
 func main() {
 	e := gin.New()
 	e.Use(gin.Logger())
 	e.Use(gin.Recovery())
 	e.GET("/gui/*filepath", static)
+	e.GET("/api/stats", stats)
 	e.Run(":1488")
 }
